ec2bd9ffd766bd9ed05cb7a0b21086c516390269
ap-reyes <adrianpauln.reyes@gmail.com>
2022-10-05 09:10:34 +0800

    updates angle calculator and refactors package name

---
+++ app/src/main/java/com/example/motion/object/PoseDetectionActivity.java
@@ -0,0 +1,33 @@
+package com.example.motion.object;
+
+import android.os.Bundle;
+
+import com.example.motion.helpers.MLVideoHelperActivity;
+import com.example.motion.helpers.vision.posedetector.PoseDetectorProcessor;
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions;
+
+public class PoseDetectionActivity extends MLVideoHelperActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void setProcessor() {
+        AccuratePoseDetectorOptions options = new AccuratePoseDetectorOptions.Builder()
+                .setDetectorMode(AccuratePoseDetectorOptions.STREAM_MODE)
+                .build();
+        cameraSource.setMachineLearningFrameProcessor(new PoseDetectorProcessor(
+                this,
+                options,
+                false,
+                false,
+                false,
+                true,
+                true,
+                true)
+        );
+    }
+}
