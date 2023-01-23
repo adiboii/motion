c1a7c58df7a571363f50106e3149e737bc0c47eb
ap-reyes <adrianpauln.reyes@gmail.com>
2023-01-23 14:38:53 +0800

    comments out fps tracker

--- app/src/main/java/com/example/motion/helpers/vision/InferenceInfoGraphic.java
+++ app/src/main/java/com/example/motion/helpers/vision/InferenceInfoGraphic.java
@@ -62,29 +62,29 @@ public class InferenceInfoGraphic extends GraphicOverlay.Graphic {
   @Override
   public synchronized void draw(Canvas canvas) {
       //return;
-    float x = TEXT_SIZE * 0.5f;
-    float y = TEXT_SIZE * 1.5f;
-
-    canvas.drawText(
-        "InputImage size: " + overlay.getImageHeight() + "x" + overlay.getImageWidth(),
-        x,
-        y + 100,
-        textPaint);
-
-    if (!showLatencyInfo) {
-      return;
-    }
-    // Draw FPS (if valid) and inference latency
-    if (framesPerSecond != null) {
-      canvas.drawText(
-          "FPS: " + framesPerSecond + ", Frame latency: " + frameLatency + " ms",
-          x,
-          y + TEXT_SIZE + 100,
-          textPaint);
-    } else {
-      canvas.drawText("Frame latency: " + frameLatency + " ms", x, y + TEXT_SIZE + 100, textPaint);
-    }
-    canvas.drawText(
-        "Detector latency: " + detectorLatency + " ms", x, y + TEXT_SIZE * 2 + 100, textPaint);
+//    float x = TEXT_SIZE * 0.5f;
+//    float y = TEXT_SIZE * 1.5f;
+//
+//    canvas.drawText(
+//        "InputImage size: " + overlay.getImageHeight() + "x" + overlay.getImageWidth(),
+//        x,
+//        y + 100,
+//        textPaint);
+//
+//    if (!showLatencyInfo) {
+//      return;
+//    }
+//    // Draw FPS (if valid) and inference latency
+//    if (framesPerSecond != null) {
+//      canvas.drawText(
+//          "FPS: " + framesPerSecond + ", Frame latency: " + frameLatency + " ms",
+//          x,
+//          y + TEXT_SIZE + 100,
+//          textPaint);
+//    } else {
+//      canvas.drawText("Frame latency: " + frameLatency + " ms", x, y + TEXT_SIZE + 100, textPaint);
+//    }
+//    canvas.drawText(
+//        "Detector latency: " + detectorLatency + " ms", x, y + TEXT_SIZE * 2 + 100, textPaint);
   }
 }
