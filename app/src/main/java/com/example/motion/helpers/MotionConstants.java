854a8e7f5ef3b908525ca5fa654e0e9133563952
Ada Pauline Villacarlos <miku_marvell@yahoo.com.ph>
2023-01-27 14:01:52 +0800

    Add yoga pose left & right

--- app/src/main/java/com/example/motion/helpers/MotionConstants.java
+++ app/src/main/java/com/example/motion/helpers/MotionConstants.java
@@ -8,15 +8,20 @@ public class MotionConstants {
     // 0 left elbow - 1 right elbow - 2 left shoulder - 3 right shoulder
     // 4 left hip - 5 right hip - 6 left knee -7 right knee
 
-    private static final int[] treeIdealAngles = {143,143,172,170,171,126,165,45};
-    private static final int[] goddessIdealAngles = {99,99,94,96,104,105,106,107};
-    private static final int[] warrior2IdealAngles = {174,175,95,101,126,101,170,112};
+    private static final int[] treeLeftIdealAngles = {145,147,172,172,121,176,31,175};
+    private static final int[] treeRightIdealAngles = {145,144,172,171,176,122,175,33};
+    private static final int[] goddessIdealAngles = {98,98,93,95,105,106,107,109};
+    private static final int[] warrior2LeftIdealAngles = {175,175,103,94,96,127,104,176};
+    private static final int[] warrior2RightIdealAngles = {175,175,94,102,127,97,176,106};
+
     public static final  Map<String, int[]> idealAnglesMap = new HashMap<>();
 
     static{
-        idealAnglesMap.put("tree", treeIdealAngles);
+        idealAnglesMap.put("tree-left", treeLeftIdealAngles);
+        idealAnglesMap.put("tree-right",treeRightIdealAngles);
         idealAnglesMap.put("goddess", goddessIdealAngles);
-        idealAnglesMap.put("warrior2", warrior2IdealAngles);
+        idealAnglesMap.put("warrior2-left", warrior2LeftIdealAngles);
+        idealAnglesMap.put("warrior2-right",warrior2RightIdealAngles);
     }
 
 }
