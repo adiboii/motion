511c022c39b27d9b9f400e196338a86730f6400e
Ada Pauline Villacarlos <miku_marvell@yahoo.com.ph>
2023-02-23 13:36:39 +0800

    Add History UI

--- app/src/main/java/com/example/motion/MainActivity.java
+++ app/src/main/java/com/example/motion/MainActivity.java
@@ -2,20 +2,49 @@ package com.example.motion;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.motion.object.PoseDetectionActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnStart;
+    Button btnHistory;
+    Button selectedButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btn_start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openPoseDetectionActivity();
+            }
+        });
+        btnHistory = findViewById(R.id.btn_history);
 
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHistoryActivity();
+            }
+        });
+    }
+
+    public void openPoseDetectionActivity(){
         // Automatically redirects to Camera Screen
         Intent intent = new Intent(this, PoseDetectionActivity.class);
         startActivity(intent);
     }
+
+    public void openHistoryActivity(){
+        Intent intent = new Intent(this, History.class);
+        startActivity(intent);
+    }
+
 }
